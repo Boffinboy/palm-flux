46436a0f880f161d976e2e4410a788615a64869d
Boffinboy <benmaddox@gmail.com>
2023-09-24 15:17:57 +0100

    Fixed indent

--- palm.py
+++ palm.py
@@ -560,7 +560,7 @@ if __name__ == '__main__':
                     inv_cmd = inverter.compute_tgt_soc(pv_forecast, stgs.Solcast.weight, True)
                     if inv_cmd == "set_soc":
                         inverter.tgt_soc = str(max(int(inverter.soc), int(inverter.tgt_soc)))
-                       inverter.set_mode(inv_cmd) 
+                    inverter.set_mode(inv_cmd) 
                 except Exception as error:
                     logger.error(str(type(error).__name__))
                     logger.error(str(error))
